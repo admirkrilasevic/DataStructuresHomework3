e9a9eb7fbeb26e812d55f315023c2e06d27ef88f
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-28 21:44:03 +0200

    final changes

---
+++ README.md
@@ -0,0 +1,4 @@
+### **Homework 3 assignment from the Data Structures course:**
+- working with complex data (from .csv file)
+1. Implementing bubble sort algorithm
+2. Implementing binary search algorithm
---
+++ src/hw3/binarysearch/BinarySearch.java
@@ -0,0 +1,22 @@
+package hw3.binarysearch;
+
+//binary search implementation using the overriden compareTo method
+public class BinarySearch {
+	
+	public static int search(IPAddress[] addresses, long number) {
+	    int low = 0;                                   
+	    int high = addresses.length - 1;                 
+	        
+	    while (low <= high) {                           
+	        int mid = low + (high - low) / 2;           
+	        if (addresses[mid].compareTo(number) == -1) {                  
+	            high = mid - 1;                         
+	        } else if (addresses[mid].compareTo(number) == 1) {           
+	            low = mid + 1;                          
+	        } else {                                    
+	            return mid;                             
+	        }
+	    }
+	    return -1;                                      
+	}
+}
